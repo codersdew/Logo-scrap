71f26302845ab6d6682fcdaf23557e2b0beb442b
DEWCODERS <codersdew@gmail.com>
2026-02-24 06:32:53 +0530

    🩶 KEZU TECH ☠️

--- api/index.js
+++ api/index.js
@@ -1,15 +1,24 @@
+`* dphn ai scraper`
+
+`* Scraper by - Thishan boy dev`
+
+`* scraped site - chat.dphn.ai`
+
+______________________
+
 const axios = require('axios');
 
-export default async function handler(req, res) {
-    const { question, template = 'logical' } = req.query; // URL එකෙන් දත්ත ලබා ගැනීම
-
-    if (!question) {
-        return res.status(400).json({ error: 'Question is required' });
-    }
-
+async function dolphinai(question, { template = 'logical' } = {}) {
     try {
+        const templates = ['logical', 'creative', 'summarize', 'code-beginner', 'code-advanced'];
+        if (!question) throw new Error('Question is required.');
+        if (!templates.includes(template)) throw new Error(`Available templates: ${templates.join(', ')}.`);
+        
         const { data } = await axios.post('https://chat.dphn.ai/api/chat', {
-            messages: [{ role: 'user', content: question }],
+            messages: [{
+                role: 'user',
+                content: question
+            }],
             model: 'dolphinserver:24B',
             template: template
         }, {
@@ -19,15 +28,15 @@ export default async function handler(req, res) {
                 'user-agent': 'Mozilla/5.0 (Linux; Android 15; SM-F958 Build/AP3A.240905.015) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.6723.86 Mobile Safari/537.36'
             }
         });
-
-        const result = data.split('\n\n')
-            .filter(line => line && line.startsWith('data: {'))
-            .map(line => JSON.parse(line.substring(6)))
-            .map(line => line.choices[0].delta.content)
-            .join('');
-
-        res.status(200).json({ status: true, result });
+        
+        const result = data.split('\n\n').filter(line => line && line.startsWith('data: {')).map(line => JSON.parse(line.substring(6))).map(line => line.choices[0].delta.content).join('');
+        if (!result) throw new Error('No result found.');
+        
+        return result;
     } catch (error) {
-        res.status(500).json({ status: false, error: error.message });
+        throw new Error(error.message);
     }
 }
+
+// Usage:
+dolphinai('hi!').then(console.log);
